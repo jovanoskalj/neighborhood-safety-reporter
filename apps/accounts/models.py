3c975944d298a15cc6b174d56cdc298116a825d0
Magdalena Rizova <magdalenarizova03@gmail.com>
2026-03-26 17:18:24 +0100

    Update

---
+++ apps/accounts/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser, User
+from django.db import models
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('citizen', 'Citizen'),
+        ('officer', 'Officer'),
+        ('admin', 'Administrator')
+    ]
+
+    SECTOR_CHOICES = [
+        ('infrastructure', 'Infrastructure'),
+        ('utilities', 'Utilities'),
+        ('safety', 'Safety'),
+        ('health', 'Health'),
+        ('admin', 'Administration')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='citizen')
+    sector = models.CharField(max_length=50, choices=SECTOR_CHOICES, blank=True)  # only for officers
+    phone = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'reports',
-    'ai',
+    'apps.accounts',
+    'apps.reports',
+    'apps.ai_classifier',
+    
 ]
 
 MIDDLEWARE = [
@@ -77,12 +78,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'safety_reporter',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
