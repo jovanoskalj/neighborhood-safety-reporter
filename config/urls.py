f02464ce8a18be264f8bc4e72630d2a3179aeb09
et27 <elena.taneva@students.finki.ukim.mk>
2026-04-17 12:02:52 +0200

    T15: officer work panel - split layout, filters, map, status update

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,4 +9,4 @@ urlpatterns = [
     path("accounts/", include("apps.accounts.urls")),
     path("verify/", include("verify_email.urls")),
     path("verification/", include("verify_email.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
