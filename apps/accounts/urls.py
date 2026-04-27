126ce5d3c35c7772cf63fe83a014ce940cc43ad9
Nikola <serafimovnikola582@gmail.com>
2026-04-27 23:14:41 +0200

    fixes

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import (
     login_view, logout_view, profile_view, register_view, verify_email_code_view,
     admin_user_list, admin_user_toggle, admin_user_update_role,
